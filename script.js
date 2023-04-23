654ac8bf74917bab6c97a387f00278b8d228905b
Boba02 <124049085+Boba02@users.noreply.github.com>
2023-04-23 18:28:37 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,139 @@
+//declaring variables
+let headers=["Poori", "Samosa", "Panipuri", "Chicken Pakora", "Poppadoms", "Veg Pakora", "Masala", "Jalfrezi", "Bhuna", "Vindaloo", "Korma", "Paneer"];
+let images=["Poori.jpg", "samosa.jpg", "panipuri.jpg", "chickpakora.jpg", "poppadoms.jpg", "vegpakora.jpg", "masala.jpg", "jalfrezi.jpg", "bhuna.jpg", 
+            "Lamb-Vindaloo.jpg", "korma.jpg", "paneer.jpg"];
+let content=[["In a mixing bowl or on your work-surface, take the whole wheat flour, salt and oil.", 
+"Add little water at a time and knead well to form a dough.The dough should not be soft but stiff and tight. You can also make a semi-soft dough.", 
+"Divide the dough into small or medium pieces, about 25 to 30. Make into medium sized or slightly small balls.", 
+"Spread a bit of oil on both sides of the dough ball. Spreading oil and not dusting with flour helps the oil to stay clean and you won`t see dark burnt flour particles inside the oil.", 
+"Roll the dough evenly into circles of about 1/4 inch, neither too thin nor thick.",
+"Place the rolled poori in a plate and cover with a clean kitchen towel, so that they don`t dry up. Heat oil in a deep frying pan or kadai. When the oil is sufficiently hot then add one poori at a time and fry gently pressing down with the frying spoon or slotted spoon in a circular motion. ", 
+"Turn over when puffed up and fry till golden brown."],
+["Heat the oil in a small non-stick pan and fry the mustard seeds for about ten seconds, or until they begin to splutter.",
+"Add the onion and ginger and cook for 2-3 minutes over a high heat. Add the peas, stir well and add the spices, mango powder, salt and a splash of water. Cook for 1-2 minutes", 
+"Then add the potatoes and coriander and cook for 2-3 minutes. Taste and adjust the seasoning.",
+"Preheat the oven to 200C/400F/Gas 6.",
+"Unroll the pastry and cover with cling film and a damp tea towel. Peel off one piece and keep the rest covered so that it doesn`t dry out. Lay the pastry sheet flat on a clean surface and brush with melted butter. Fold in one third of the pastry lengthways towards the middle. Brush again with the butter and fold in the other side to make a long triple-layered strip.",
+"Place one rounded teaspoon of the filling mixture at one end of the strip, leaving a 2cm/1in border. Take the right corner and fold diagonally to the left, enclosing the filling and forming a triangle. Fold again along the upper crease of the triangle. Keep folding in this way until you reach the end of the strip. Brush the outer surface with more butter. Place onto a baking sheet and cover while you make the rest of the samosas. Sprinkle over a few sesame seeds, if using.",
+"Bake in the centre of the oven for 30-35 minutes, or until golden and crisp, turning halfway through the cooking time."],
+["Boil the potatoes till they are cooked completely.",
+"Peel them and then chop them.",
+"Finely chop the onion if using it.",
+"In a small bowl, mix the potatoes, onions, coriander leaves, cumin powder, chaat masala powder and black salt or regular salt. Mix well and keep aside.",
+"In a blender add all the ingredients mentioned above for the pani.",
+"Add water and grind to a fine chutney.",
+"Remove the green chutney in a large bowl. Rinse the mixer jar with ½ cup water first and then add this water in the bowl. Then add ½ to ¾ cup more water.",
+"Mix well. Check the seasoning. Add more salt or jeera powder or chaat masala or jaggery if required. If you want a thin pani, you could add some water. But keep on checking the seasoning, as per your taste.",
+"Add the boondi to the pani.",
+"You can chill the pani in the fridge or add some ice cubes to it.",
+"Crack the top of the puri with a spoon.",
+"Add 2 to 3 teaspoons of the boiled potato-onion filling in the poori.",
+"Stir the green pani first and then add it in the poori. Optionally you can add some sweet chutney in the puri."],
+["Put the chicken in a large bowl, then use your hands to coat the pieces in the cornflour.",
+ "Mix in the rice flour, followed by the gram flour. Add the chillies, spices, onion, coriander and 1 tsp salt. Mix well with your hands.",
+"Gradually add around 150ml water until the ingredients have become moist and ever-so-slightly wet. You may not need all the water (adding too much will stop the pakora mixture binding).",
+"Fill a deep pan no more than a third full with vegetable oil and heat to 180C.", 
+"Squeeze a small amount of the pakora mixture together before carefully lowering into the hot oil with a spoon.", 
+"Fry for 8-10 mins, turning regularly, until cooked through and browned all over.", 
+"Set aside on a plate lined with kitchen paper while you repeat with the remaining mixture, frying in small batches.", 
+"Serve hot with a sweet chilli sauce or green chutney."],
+["Mix together the flour and salt.",
+"Once the dried ingredients are well mixed, start adding water, little by little. You want to end up with a stiff dough. Add just enough water to be able to form the dough into a relatively hard ball.",
+"Knead and pound on the dough for several minutes.",
+"Divide the dough in half to make it more workable. Then, roll the dough into a thin layer around 2mm thick.",
+"Cut the dough into medium-large sized circles with a knife or circular cookie cutter",
+"Continue rolling out all of the dough until it has all been cut into thin circular disks.",
+"allow to air dry for a while",
+"deep fry the poppadoms and serve"],
+["Put the choped vegetables in a large bowl and bunch together to form `veggie balls`, then use your hands to coat the pieces in the cornflour.",
+"Mix in the rice flour, followed by the gram flour. Add the chillies, spices, onion, coriander and 1 tsp salt. Mix well with your hands.",
+"Gradually add around 150ml water until the ingredients have become moist and ever-so-slightly wet. You may not need all the water (adding too much will stop the pakora mixture binding).",
+"Fill a deep pan no more than a third full with vegetable oil and heat to 180C.", 
+"Squeeze a small amount of the pakora mixture together before carefully lowering into the hot oil with a spoon.", 
+"Fry for 8-10 mins, turning regularly, until cooked through and browned all over.", 
+"Set aside on a plate lined with kitchen paper while you repeat with the remaining mixture, frying in small batches.", 
+"Serve hot with a sweet chilli sauce or green chutney."],
+["First prepare the ginger garlic paste. Blend the ginger and garlic with a teaspoon of water in a blender. Store any you don`t use for this recipe in the fridge.",
+"To make the masala sauce, heat the oil in a large frying pan. Add the whole spices (black cardamom, peppercorns, cloves, green cardamom, cumin seeds, cinnamon and bay leaf) and fry until they start to crackle, then add the onions.",
+"Cook the onions gently and slowly until caramelised and very dark brown.",
+"Add a tablespoon of the ginger garlic paste and fry gently for 3-4 minutes. Taste to make sure the ginger and garlic are well cooked otherwise their flavour will be too strong.",
+"Add all the powdered spices and mix well.",
+"Add 100-150ml water if the sauce is too thick.",
+"Finally add the tomato puree and cook until the oil comes to the surface of the sauce."],
+["Heat the oil in a pan and fry the onion and garlic over a low to medium heat for 4/5 minutes, or until softened.",
+"Mix the chilli powder, turmeric and salt together in a bowl until well combined, then add the chicken pieces and mix well to coat. Add the coated chicken to the pan and fry for 10–15 minutes, or until golden-brown and cooked through.",
+"Stir the tomatoes, ginger, cumin and coriander into the pan, then reduce the heat until the mixture is just simmering. Cover the pan with a lid and cook for 20/30 minutes, stirring occasionally and adding a tablespoon of water every so often if the mixture appears too dry.",
+"Stir in the butter and add the lemon juice, to taste.",
+"To serve, spoon the chicken jalfrezi onto serving plates. Serve with rice, naan and lime wedges on the side, then garnish with the chopped coriander and sliced red chillies."],
+["To make the marinade, blend the garlic, ginger and a splash of water to a smooth paste in a food processor or blender. Put in a large bowl with the remaining marinade ingredients, except the chicken, and mix to combine. Add the chicken to the bowl and mix well. Leave to marinate for 1 hour, or overnight preferably.",
+"To make the bhuna, blend the tomatoes and tomato purée to a smooth paste in a food processor or blender, then set aside. Heat the oil in a non-stick kadhai or large lidded frying pan over a medium heat. Add the chillies, fry for a few seconds then add the onions. Stir well and fry gently for 25 minutes, until browned and softened. Keep stirring often to make sure they cook evenly.",
+"Add the blended tomato paste, stir and fry for 5 minutes. Add the marinated chicken and cook for 6 minutes, stirring so the chicken is well coated in the masala. Season with salt, lower the heat and continue cooking with a lid on for 8 minutes until the chicken is cooked through, stirring halfway through. Add the fennel, pepper and garam masala and stir. Garnish with coriander and cook for a couple of minutes. Serve immediately."],
+["To prepare the chicken, mix the lemon juice, diced chicken, vinegar, garlic and ginger together in a large bowl with the dried spices. Cover and leave to marinate in the fridge for at least 3-4 hours, or overnight.",
+"Heat the oil in a large saucepan over medium-high heat and add the spices, onion and salt.",
+"Cook, stirring, until the onion is browned but not burnt, about 10-15 mins. Reduce the heat to medium and add the marinated chicken.", 
+"Cook, stirring, for another 5 mins. Add a splash of water, reduce the heat slightly, cover and simmer for 15-20 mins until the chicken is cooked through.",
+"Garnish with some coriander and crispy onions and serve with flatbreads, if you like."],
+["Put the chicken thighs in a bowl with the turmeric, lime or lemon juice, oil and salt. Toss together well and set aside for 10 minutes.",
+"To make the sauce, heat the oil or ghee in a saucepan and add the cumin seeds.", 
+"Once they start to sizzle, add the chopped onion, garlic and ginger and cook gently for 5/6 minutes over a low heat, or until well softened, stirring regularly.",
+"Tip the tomatoes and tomato purée into the pan and cook for another 2 minutes, stirring. Next, stir in the turmeric, garam masala, chilli powder, black pepper and a teaspoon of salt.", 
+"Add the ground almonds and milk and cook on a low heat for 10 minutes, or until thickened, stirring regularly.",
+"While the sauce is cooking, heat a griddle pan or heavy-based frying pan.",
+"Cook the chicken pieces for 8/10 minutes, or until lightly browned and cooked through, turning occasionally.",
+"Add the chicken to the sauce, cover loosely and simmer gently together for 5/6 minutes, stirring regularly. It’s important to cover the pan as the sauce will splatter a little as it cooks. If it becomes too thick, add a splash of water.", 
+"If you have time, leave the curry to stand for 30 minutes before serving, then reheat gently.", 
+"Serve with rice on the side and fresh coriander scattered over."],
+["Heat the oil in a saucepan or large, lidded frying pan over a medium heat. Once hot, fry the paneer for two minutes on each side, or until the cubes are coloured. Remove from the pan and set aside.",
+"Add the onions with a pinch of salt and gently fry until golden-brown.", 
+"Once golden, add the garlic, chilli flakes, curry powder and two tablespoons of water. Gently fry for a further two minutes over a low heat. Stir in the potato.",
+"Pour in the tomatoes and the stock, bring to a simmer, cover with a lid and simmer gently for 15 minutes, or until the potatoes are completely soft and the sauce has thickened.",
+"Meanwhile, cook the rice according to packet instructions.",
+"Add the paneer to the curry and stir through, making sure it is well coated in sauce. Heat through for 3-4 minutes then serve with the rice."]
+];
+let ingredients=[
+                    ["3 cups whole wheat flour","1 teaspoon oil","1 teaspoon sal","3/4 to 1 cup water","oil"],
+                    ["vegetable oil","mustard seeds","chopped onion","finely chopped ginger","frozen peas","ground coriander","ground cumin","red chilli powder","garam masala","dried mango powder ","salt","splash water","potatoes, peeled, boiled until soft and crushed into large lumps","chopped fresh coriander leaves","packet ready-made filo pastry","melted butter","sesame seeds"],
+                    ["3 potatoes","1 onion", "chopped coriander leaves", "roasted cumin powder","chaat masala powder","red chili powder", "salt","chopped mint leaves","ginger", "3 green chilies","tamarind","jaggery powder","water","boondi"],
+                    ["700g boneless chicken, cut into bite-sized pieces","2 tbsp cornflour","50g rice flour","40g gram flour, sieved","3 dried chillies","chilli powder","curry powder","ground coriander","cumin","2 small onions","small bunch of coriander, finely chopped","vegetable oil","sweet chilli sauce or green chutney"],
+                    ["urad flour","water","salt","oil"],
+                    ["2 tbsp cornflour","50g rice flour","40g gram flour, sieved","3 dried chillies","chilli powder","curry powder","ground coriander","cumin","2 small onions","small bunch of coriander, finely chopped","vegetable oil","sweet chilli sauce or green chutney"],
+                    ["vegetable oil","black cardamom","black peppercorns","cloves","green cardamom","cumin seeds","cinnamon sticks","bay leaf","onions, chopped","ginger garlic paste","ground turmeric","red chilli powder","ground coriander","garam masala","salt","tomato puree"],
+                    ["2 tbsp vegetable oil","1 onion, finely chopped","2 cloves garlic, thinly sliced","1 tsp chilli powder","3 tsp ground turmeric","pinch salt2","750g/1lb 10oz skinless chicken breast, cut into large chunks","400g can chopped tomatoes","2 tsp ground ginger","3 tsp ground cumin","3 tsp ground coriander","2 tbsp melted butter","1 lemon, juice only"],
+                    ["6 garlic cloves, finely grated","2.5cm/1in piece fresh root ginger, peeled and finely grated","2 tsp mild chilli powder","ground turmeric",",1 tsp ground cumin","1 tbsp ground coriander","2 tbsp Greek-style yoghurt","750g/1lb 10oz chicken thighs, boneless and skin removed, cut into bite-sized pieces","tomatoes, roughly chopped","2 tbsp tomato puree","4 tbsp vegetable oil","3 dried mild red chillies","brown onions, finely chopped","1 tsp fennel seeds, coarsely crushed","freshly ground black pepper","1 tsp garam masala","salt","chopped fresh coriander"],
+                    ["4 tbsp vegetable oil","flaked cinnamon stick","chilli flakes","cayenne pepper","1 onion, finely chopped","salt","1 tbsp lemon juice","4 boneless, skinless chicken breasts, diced","2 tbsp white wine vinegar","2-3 garlic cloves, crushed","2 tsp finely grated ginger","1 tsp garam masala","1 tsp ground cumin","1 tsp ground coriander","cayenne pepper","black mustard seeds"],
+                    ["2 tbsp sunflower oil or 1 tbsp ghee","1 tsp cumin seeds","1 medium onion, very finely chopped","4 garlic cloves, finely chopped","2 tsp finely grated fresh root ginger","can chopped tomatoes, blended until smooth","1 tsp tomato purée","1 tsp ground turmeric","1 heaped tsp garam masala","hot chilli powder","5gground almonds","300ml pint full-fat milk","coarsely ground black pepper","4 boneless, skinless chicken thighs, trimmed of excess fat and quartered","½ tsp ground turmeric","1 tbsp lime or lemon juice","2 tsp rapeseed or sunflower oil","½ tsp fine sea salt"],
+                    ["2 tbsp olive oil","200g paneer, cubed","2 onions, finely sliced","2 garlic cloves, crushed","chilli flakes","mild curry powder","250g floury potatoes, peeled and cut into small cubes","200g tinned tomatoes","500ml vegetable stock (made from stock cube)","200g rice","salt and freshly ground black pepper"]
+];
+
+function getIndex(index){
+    localStorage.setItem("value", index)
+}
+
+let i = localStorage.getItem("value")
+document.getElementById("content").innerHTML = `
+<div id="first">
+    <h2>steps</h2>
+    <ol id="recipes"></ol>
+</div>
+<div id="ingreed">
+    <h2>Ingredients</h2>
+    <ul id="list"></ul>
+</div>
+<img id="mains" src="images/${images[i]}" alt="down">`;
+
+document.getElementById("headers").innerHTML = headers[i];
+document.getElementById("title").innerHTML = headers[i];
+
+let list = document.getElementById("list");
+for(item in ingredients[i]){
+    let li = document.createElement("li");
+    li.innerHTML = ingredients[i][item];
+    list.appendChild(li);
+}
+
+let contList = document.getElementById("recipes");
+for(item in content[i]){
+    let li = document.createElement("li");
+    li.innerHTML = content[i][item];
+    contList.appendChild(li);
+}
